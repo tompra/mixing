6d0389da9b62d4f9138fbefd878c1a8871838a28
tompra <tomprayon@gmail.com>
2024-05-14 12:53:10 +0200

    create wrapper layout for out components

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,4 +1,22 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import styled from 'styled-components';
+
 const Layout: React.FC = (): JSX.Element => {
-    return <h1>Layout</h1>;
+    return (
+        <>
+            <Navbar />
+            <Wrapper>
+                <Outlet />
+            </Wrapper>
+        </>
+    );
 };
+
+const Wrapper = styled.section`
+    width: 90vw;
+    max-width: 1120px;
+    margin: 0 auto;
+    padding: 5rem 2rem;
+`;
 export default Layout;
