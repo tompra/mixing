e5513b585fb9fa401052426999de84903b8cb50e
tompra <tomprayon@gmail.com>
2024-05-14 11:53:27 +0200

    manage component import through index

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export { default as Home } from './Home';
+export { default as About } from './About';
+export { default as Recipe } from './Recipe';
+export { default as Error } from './Error';
+export { default as Layout } from './Layout';
+export { default as Newsletter } from './Newsletter';
