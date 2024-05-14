a1b2f393c4b05697d921a7b807a5a5e87ab1abc3
tompra <tomprayon@gmail.com>
2024-05-14 11:53:54 +0200

    create router for path and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,34 @@
-const App = () => {
-    return <div>App</div>;
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { About, Layout, Home, Error, Newsletter, Recipe } from './pages/index';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+    },
+    {
+        path: '/about',
+        element: <About />,
+    },
+    {
+        path: '/home',
+        element: <Home />,
+    },
+    {
+        path: '/newsletter',
+        element: <Newsletter />,
+    },
+    {
+        path: '/recipe',
+        element: <Recipe />,
+    },
+    {
+        path: '/error',
+        element: <Error />,
+    },
+]);
+
+const App: React.FC = (): JSX.Element => {
+    return <RouterProvider router={router} />;
 };
 export default App;
