d37079f1fd8414ca87e99e716002a6e59373aeb5
tompra <tomprayon@gmail.com>
2024-05-14 12:52:40 +0200

    about page with styled components

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,24 @@
+import styled from 'styled-components';
+
 const About: React.FC = (): JSX.Element => {
-    return <h1>About</h1>;
+    return (
+        <Wrapper>
+            <h1>About us</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reprehenderit fugiat deleniti harum enim ullam est minima
+                mollitia deserunt quos rerum repellendus nesciunt non neque
+                illum animi quo, reiciendis quidem aperiam.
+            </p>
+        </Wrapper>
+    );
 };
+
+const Wrapper = styled.div`
+    p {
+        line-height: 2;
+        color: grey;
+        margin-top: 2rem;
+    }
+`;
 export default About;
