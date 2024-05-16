f57debfc3ffc4f614c45d32e35188f815c975567
tompra <tomprayon@gmail.com>
2024-05-16 11:31:10 +0200

    fetch data from home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,25 @@
+import axios from 'axios';
+import { useLoaderData } from 'react-router-dom';
+import RecipeList from '../components/RecipeList';
+
+const API_KEY = import.meta.env.VITE_ACCESS_KEY;
+
+const recipeSearchUrl = `https://api.spoonacular.com/recipes/complexSearch?query=`;
+const apiURL = `&information&apiKey=${API_KEY}`;
+
+export const loader = async () => {
+    const searchTerm = 'pasta';
+    const response = await axios.get(
+        `${recipeSearchUrl}${searchTerm}${apiURL}`
+    );
+    return { recipes: response.data.results, searchTerm };
+};
 const Home: React.FC = (): JSX.Element => {
-    return <h1>Home</h1>;
+    const data = useLoaderData() as { recipes?: [] };
+    return (
+        <>
+            <RecipeList recipes={data.recipes} />
+        </>
+    );
 };
 export default Home;
