be3d5df851e1726550440d63b085bb94358a11e4
tompra <tomprayon@gmail.com>
2024-05-17 12:45:18 +0200

    add recipe id param

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,15 +1,21 @@
+import { Link } from 'react-router-dom';
+
 type Props = {
     recipe: { id: number; title: string; image: string };
 };
 
 const RecipeCard: React.FC<Props> = ({ recipe }): JSX.Element => {
-    console.log('recipe', recipe);
     const { id, title, image } = recipe;
 
     return (
-        <div key={id}>
-            <p>{title}</p>
-            <img src={image} alt={title} />
+        <div>
+            <div>
+                <img src={image} alt={title} />
+            </div>
+            <div>
+                <h3>{title}</h3>
+            </div>
+            <Link to={`/recipe/${id}`}>More information</Link>
         </div>
     );
 };
