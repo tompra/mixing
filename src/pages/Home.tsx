2a5e1a2ed5ae1b1b64d0f998e55956a48ed08b83
tompra <tomprayon@gmail.com>
2024-05-23 17:16:18 +0200

    import searchTerm from useLoader

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,18 @@
 import { useLoaderData } from 'react-router-dom';
 import RecipeList from '../components/RecipeList';
+import SearchForm from '../components/SearchForm';
+import { Recipe } from '../helpers/types';
 
 const Home: React.FC = (): JSX.Element => {
-    const data = useLoaderData() as { recipes?: [] };
-    return <RecipeList recipes={data.recipes} />;
+    const { recipes, searchTerm } = useLoaderData() as {
+        recipes: Recipe[];
+        searchTerm: string;
+    };
+    return (
+        <>
+            <SearchForm searchTerm={searchTerm} />
+            <RecipeList recipes={recipes} />
+        </>
+    );
 };
 export default Home;
