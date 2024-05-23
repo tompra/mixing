f0569dd5d30546fec33bea890f7ca6e0bbf7ac7c
tompra <tomprayon@gmail.com>
2024-05-23 16:48:46 +0200

    create wrapper for search form

---
+++ src/assets/wrappers/SearchWrap.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const SearchWrapper = styled.div`
+    margin-bottom: 6rem;
+    .form {
+        display: grid;
+        grid-template-columns: 1fr auto;
+    }
+    .form-input {
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+    }
+    .btn {
+        border-top-left-radius: 0;
+        border-bottom-left-radius: 0;
+    }
+`;
