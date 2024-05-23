12ffcfcee95772b9fdf5b396b4fca575c6eb2d1f
tompra <tomprayon@gmail.com>
2024-05-23 16:38:29 +0200

    import props

--- src/helpers/loaders.tsx
+++ src/helpers/loaders.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { LoaderParams, Recipe } from './types';
 
 const API_KEY = import.meta.env.VITE_ACCESS_KEY;
 
@@ -11,10 +12,10 @@ export const homeLoader = async () => {
     const response = await axios.get(
         `${recipeSearchUrl}${searchTerm}${apiURL}`
     );
-    return { recipes: response.data.results, searchTerm };
+    return { recipes: response.data.results as Recipe[], searchTerm };
 };
 
-export const recipeLoader = async ({ params }) => {
+export const recipeLoader = async ({ params }): Promise<LoaderParams> => {
     const { id } = params;
     // const instructionURL = `${recipeInstructionsURL}/${id}/analyzedInstructions?apiKey=${API_KEY}`;
     const informationURL = `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=true&apiKey=${API_KEY}`;
