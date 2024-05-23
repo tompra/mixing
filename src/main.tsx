b87a82e092aab460e02c71ebe448ec44a14d13ff
tompra <tomprayon@gmail.com>
2024-05-23 16:37:13 +0200

    import toastify

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import React from 'react';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <ToastContainer position='top-center' autoClose={2500} />
+        <App />
+    </React.StrictMode>
+);
