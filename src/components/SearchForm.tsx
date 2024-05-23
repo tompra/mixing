1a2bcdadbd0dd51b9b6c15d52440e97a217ef70f
tompra <tomprayon@gmail.com>
2024-05-23 16:48:35 +0200

    Create search form component

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,4 +1,21 @@
+import { useNavigation, Form } from 'react-router-dom';
+import { SearchWrapper } from '../assets/wrappers/SearchWrap';
 const SearchForm: React.FC = (): JSX.Element => {
-    return <div>SearchForm</div>;
+    return (
+        <SearchWrapper>
+            <Form className='form'>
+                <input
+                    type='search'
+                    name='search'
+                    id='search'
+                    className='form-input'
+                    defaultValue='meat'
+                />
+                <button type='submit' className='btn'>
+                    Search
+                </button>
+            </Form>
+        </SearchWrapper>
+    );
 };
 export default SearchForm;
