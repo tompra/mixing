10da8889ad14c7ceedaa9b93cbe72b1fd6075e02
tompra <tomprayon@gmail.com>
2024-05-23 16:42:12 +0200

    newsletter form functionality

--- src/pages/Newsletter.tsx
+++ src/pages/Newsletter.tsx
@@ -1,4 +1,62 @@
+import { Form, useNavigation } from 'react-router-dom';
+
 const NewsLetter: React.FC = (): JSX.Element => {
-    return <h1>NewsLetter</h1>;
+    const navigation = useNavigation();
+    const isSubmitting = navigation.state === 'submitting';
+
+    return (
+        <Form className='form' method='POST'>
+            <h3 style={{ textAlign: 'center', marginBottom: '2rem' }}>
+                our newsletter
+            </h3>
+            <div className='form-row'>
+                <label htmlFor='name' className='form-label'>
+                    name
+                </label>
+                <input
+                    type='text'
+                    name='name'
+                    id='name'
+                    className='form-input'
+                    defaultValue='Name'
+                    required
+                />
+            </div>
+            <div className='form-row'>
+                <label htmlFor='name' className='form-label'>
+                    Last Name
+                </label>
+                <input
+                    type='text'
+                    name='lastName'
+                    id='lastName'
+                    className='form-input'
+                    defaultValue='Last Name'
+                    required
+                />
+            </div>
+            <div className='form-row'>
+                <label htmlFor='email' className='form-label'>
+                    email
+                </label>
+                <input
+                    type='text'
+                    name='email'
+                    id='email'
+                    className='form-input'
+                    defaultValue='test@test.com'
+                    required
+                />
+            </div>
+            <button
+                type='submit'
+                className='btn btn-block'
+                style={{ marginTop: '0.5rem' }}
+                disabled={isSubmitting}
+            >
+                {isSubmitting ? 'submitting' : 'submit'}
+            </button>
+        </Form>
+    );
 };
 export default NewsLetter;
