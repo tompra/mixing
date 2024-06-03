261e9d0c757a627587d3dcfe9b4b00195bfa5435
tompra <tomprayon@gmail.com>
2024-06-03 19:37:10 +0200

    apply query client to home loader

--- src/helpers/loaders.tsx
+++ src/helpers/loaders.tsx
@@ -4,26 +4,33 @@ import { LoaderParams, Recipe } from './types';
 const API_KEY = import.meta.env.VITE_ACCESS_KEY;
 
 const recipeSearchUrl = `https://api.spoonacular.com/recipes/complexSearch?query=`;
-// const recipeInstructionsURL = `https://api.spoonacular.com/recipes/`;
 const apiURL = `&information&apiKey=${API_KEY}`;
 
-export const homeLoader = async ({ request }) => {
-    const url = new URL(request.url);
-
-    const searchTerm = url.searchParams.get('search') || '';
-    const response = await axios.get(
-        `${recipeSearchUrl}${searchTerm}${apiURL}`
-    );
-    return { recipes: response.data.results as Recipe[], searchTerm };
+export const searchRecipeQuery = (searchTerm: string) => {
+    return {
+        queryKey: ['search', searchTerm || 'all'],
+        queryFn: async () => {
+            const response = await axios.get(
+                `${recipeSearchUrl}${searchTerm}${apiURL}`
+            );
+            return response.data.results as Recipe[];
+        },
+    };
 };
+
+export const homeLoader =
+    (queryClient) =>
+    async ({ request }) => {
+        const url = new URL(request.url);
+        const searchTerm = url.searchParams.get('search') || '';
+        await queryClient.ensureQueryData(searchRecipeQuery(searchTerm));
+        return { searchTerm };
+    };
 
 export const recipeLoader = async ({ params }): Promise<LoaderParams> => {
     const { id } = params;
-    // const instructionURL = `${recipeInstructionsURL}/${id}/analyzedInstructions?apiKey=${API_KEY}`;
     const informationURL = `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=true&apiKey=${API_KEY}`;
-
     try {
-        // const { data } = await axios.get(instructionURL);
         const { data } = await axios.get(informationURL);
         return { id, data };
     } catch (error) {
