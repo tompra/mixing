12508ac6ce584d880572293d7f0255ed7f6231c2
tompra <tomprayon@gmail.com>
2024-06-03 15:52:12 +0200

    create a wrapper for loading component

---
+++ src/assets/wrappers/LoadingWrap.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const LoadingWrapper = styled.div`
+    width: 6rem;
+    height: 6rem;
+    border: 5px solid var(--grey-400);
+    border-radius: 50%;
+    border-top-color: var(--primary-500);
+    animation: spinner 0.6s linear infinite;
+    margin: 0 auto;
+
+    @keyframes spinner {
+        to {
+            transform: rotate(360deg);
+        }
+    }
+`;
