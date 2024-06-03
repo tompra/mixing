2345ca55044fc35fb3641d23faab8f50bca43f76
tompra <tomprayon@gmail.com>
2024-06-03 19:36:38 +0200

    apply media viewports

--- src/assets/wrappers/RecipeCardWrap.ts
+++ src/assets/wrappers/RecipeCardWrap.ts
@@ -27,4 +27,40 @@ export const RecipeCardWrapper = styled.article`
         align-items: center;
         justify-content: space-between;
     }
+    @media (max-width: 1200px) {
+        width: 60%;
+    }
+
+    @media (max-width: 992px) {
+        width: 75%;
+    }
+
+    @media (max-width: 768px) {
+        width: 90%;
+        margin: 2rem 1rem;
+
+        img {
+            height: 12rem;
+        }
+
+        .footer {
+            padding: 1rem;
+            flex-direction: column;
+            align-items: flex-start;
+        }
+
+        .footer-title {
+            width: 100%;
+            margin-bottom: 1rem;
+        }
+    }
+
+    @media (max-width: 576px) {
+        width: 100%;
+        margin: 1rem 0;
+
+        img {
+            height: 10rem;
+        }
+    }
 `;
