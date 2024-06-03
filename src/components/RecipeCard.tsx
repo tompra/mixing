cc376c0a42e2ad4aeaed2371bd2796f533f075a1
tompra <tomprayon@gmail.com>
2024-06-03 15:53:55 +0200

    truncate title and apply click to see more functionality

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,9 +1,19 @@
 import { Link } from 'react-router-dom';
 import { RecipeCardWrapper } from '../assets/wrappers/RecipeCardWrap';
 import { RecipeCardProps } from '../helpers/types';
+import { useState } from 'react';
 
 const RecipeCard: React.FC<RecipeCardProps> = ({ recipe }): JSX.Element => {
+    const [isExpanded, setIsExpanded] = useState<boolean>(false);
     const { id, title, image } = recipe;
+
+    const toggleToSeeMore = () => {
+        if (title.length < 30) return;
+        setIsExpanded(!isExpanded);
+    };
+
+    const truncatedText =
+        title.length > 30 ? `${title.substring(0, 30)}...` : title;
 
     return (
         <RecipeCardWrapper key={id}>
@@ -11,10 +21,12 @@ const RecipeCard: React.FC<RecipeCardProps> = ({ recipe }): JSX.Element => {
                 <img src={image} alt={title} className='img' />
             </div>
             <div className='footer'>
-                <h3>{title}</h3>
+                <h5 className='footer-title' onClick={toggleToSeeMore}>
+                    {isExpanded ? title : truncatedText}
+                </h5>
 
                 <button className='btn'>
-                    <Link to={`/recipe/${id}`}>More information</Link>
+                    <Link to={`/recipe/${id}`}>See more</Link>
                 </button>
             </div>
         </RecipeCardWrapper>
