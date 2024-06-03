2e54e9388520d79522237db4ab8786dcfc8fa79d
tompra <tomprayon@gmail.com>
2024-06-03 15:54:27 +0200

    add more content text

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,12 +3,42 @@ import { AboutWrapper } from '../assets/wrappers/AboutWrap';
 const About: React.FC = (): JSX.Element => {
     return (
         <AboutWrapper>
-            <h1>About us</h1>
+            <h1 className='about-title'>About us</h1>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Reprehenderit fugiat deleniti harum enim ullam est minima
-                mollitia deserunt quos rerum repellendus nesciunt non neque
-                illum animi quo, reiciendis quidem aperiam.
+                Are creatures of the cosmos Apollonius of Perga shores of the
+                cosmic ocean a mote of dust suspended in a sunbeam a billion
+                trillion a still more glorious dawn awaits. Network of wormholes
+                Sea of Tranquility from which we spring citizens of distant
+                epochs great turbulent clouds the sky calls to us? Made in the
+                interiors of collapsing stars dispassionate extraterrestrial
+                observer gathered by gravity descended from astronomers bits of
+                moving fluff paroxysm of global death.
+            </p>
+            <p>
+                Made in the interiors of collapsing stars finite but unbounded
+                brain is the seed of intelligence culture dispassionate
+                extraterrestrial observer a very small stage in a vast cosmic
+                arena. Tendrils of gossamer clouds rings of Uranus star stuff
+                harvesting star light intelligent beings bits of moving fluff
+                vastness is bearable only through love? Descended from
+                astronomers the sky calls to us laws of physics not a sunrise
+                but a galaxyrise a mote of dust suspended in a sunbeam globular
+                star cluster and billions upon billions upon billions upon
+                billions upon billions upon billions upon billions.
+            </p>
+            <p>
+                Brain is the seed of intelligence Cambrian explosion gathered by
+                gravity Hypatia great turbulent clouds across the centuries. Are
+                creatures of the cosmos billions upon billions inconspicuous
+                motes of rock and gas cosmic ocean descended from astronomers
+                are creatures of the cosmos. At the edge of forever vanquish the
+                impossible hearts of the stars at the edge of forever
+                dispassionate extraterrestrial observer dispassionate
+                extraterrestrial observer. Permanence of the stars the sky calls
+                to us not a sunrise but a galaxyrise a mote of dust suspended in
+                a sunbeam Orion's sword a mote of dust suspended in a sunbeam
+                and billions upon billions upon billions upon billions upon
+                billions upon billions upon billions.
             </p>
         </AboutWrapper>
     );
